c30697c588e387a0c3fdd0eb12fdde7a9ec1c27e
kongchakrabhat <kongchakrabhat@gmail.com>
2025-06-17 04:11:45 +0000

    add password.py

--- cell02/ex02/password.py
+++ cell02/ex02/password.py
@@ -1,7 +1,7 @@
 password = "Python is awesome"
 user_pass = input("Enter the password: ")
 
-if user_password == password:
+if user_pass == password:
     print("ACCESS GRANTED")
 else:
     print("ACCESS DENIED")
